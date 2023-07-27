50f7125b77f4021cf00004401fea35ae2f2613c7
Kanaja Hunt <KanajaHunt@gmail.com>
2023-07-27 15:50:21 -0400

    added cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const PORT = 8000;
+
+app.use(cors());
 
 const rappers = {
     "21 savage": {
@@ -39,6 +42,6 @@ app.get("/api/:rapperName", (request, response) =>{
     }
 })
 
-app.listen(PORT, ()=>{
+app.listen(process.env.PORT || PORT, ()=>{
     console.log(`The server is running on port ${PORT}! You better go catch it!`);
 })
